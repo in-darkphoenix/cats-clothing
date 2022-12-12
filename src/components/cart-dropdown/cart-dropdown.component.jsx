f658e3af83921238f87b170f558f4b1c22ff32ee
ankit <ph.official.ankit@gmail.com>
2022-12-12 11:15:54 +0530

    add checkout feature

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { CartContext } from "../../contexts/cart.context";
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
@@ -7,14 +8,20 @@ import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate("/checkout");
+  };
+
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
         {cartItems.map((item) => (
-          <CartItem cartItem={item} />
+          <CartItem cartItem={item} key={item.id} />
         ))}
       </div>
-      <Button>Go To Checkout</Button>
+      <Button onClick={goToCheckoutHandler}>Go To Checkout</Button>
     </div>
   );
 };
